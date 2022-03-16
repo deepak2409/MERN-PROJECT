4f1af3ca3d9bb974b93079cb59896775df48e09b
Deepak Tiwari <deepak24294@gmail.com>
2022-03-16 22:29:47 +0530

    Pagination working

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 
 
@@ -16,9 +17,13 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
 // Get all products
 exports.getAllProducts = catchAsyncErrors( async(req,res) =>{
-    const products = await Product.find();
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
     res.status(200).json({success : true,
-        products})
+        products
+    });
 
 });
 // Get Product Details
@@ -29,7 +34,8 @@ exports.getProductDetails =  catchAsyncErrors(async(req,res,next) =>{
     }
     res.status(200).json({
         success: true,
-        product 
+        product,
+        productCount, 
      })
 })
 
