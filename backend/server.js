ca3b7eb2d82b6985bb0faae2baa883e59b3eb71f
Deepak Tiwari <deepak24294@gmail.com>
2022-03-16 13:49:04 +0530

    Product Routes completed

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const app = require("./app");
 
 
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
 
 // Config
 
 dotenv.config({path:"backend/config/config.env"});
+
+// Connecting to database
+connectDatabase();
 
 
 app.listen(process.env.PORT,()=>{
